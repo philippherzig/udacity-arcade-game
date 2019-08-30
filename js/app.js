3d1abf37d0945f201518b778715df562e20de4c6
Philipp Herzig <philipp.herzig@switzerland.com>
2019-08-30 19:09:53 +0200

    Player is moving and respect boundaries

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function () {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -10,14 +10,14 @@ var Enemy = function() {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -25,22 +25,62 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+class Player {
+    constructor(sprite) {
+        this.sprite = "images/char-boy.png"
+        this.x = 202
+        this.y = 400
+        this.xMin = 0
+        this.xMax = 404
+        this.yMin = -15
+        this.yMax = 400
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y)
+    }
+    handleInput(key) {
+        console.log(`Old Position: x = ${this.x}, y = ${this.y}`)
+        switch (key) {
+            case "left":
+                if (this.x != this.xMin) {
+                    this.x -= 101
+                }
+                break
+            case "right":
+                if (this.x != this.xMax) {
+                    this.x += 101
+                }
+                break
+            case "up":
+                if (this.y != this.yMin) {
+                    this.y -= 83
+                }
+                break
+            case "down":
+                if (this.y != this.yMax) {
+                    this.y += 83
+                }
+                break
+        }
+        console.log(`New Position: x = ${this.x}, y = ${this.y}`)
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+const player = new Player()
+player.render
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+document.addEventListener('keydown', function (event) {
+    let allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
-
-    player.handleInput(allowedKeys[e.keyCode]);
+    player.handleInput(allowedKeys[event.keyCode])
 });
